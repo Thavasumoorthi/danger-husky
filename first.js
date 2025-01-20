9dad9e4e97316a1f554937da8d21fcc873c6bdec
Harshini B <harshini.b@tringapps.com>
2025-01-20 15:34:20 +0530

    added

--- first.js
+++ first.js
@@ -2,3 +2,13 @@ const a=10
 
 console.log("added console log")
 console.log("added console log")
+
+const b=10
+
+console.log("added console log")
+console.log("added console log")
+
+const c=10
+
+console.log("added console log")
+console.log("added console log")
